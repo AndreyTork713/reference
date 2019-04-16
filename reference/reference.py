962da81d9f87b185fc8d7ad0384f4a076cc35db8
Andrey Kropotin <tork713@gmail.com>
2019-04-16 17:00:52 +0700

    16.04.2019 17:00

--- reference/reference.py
+++ reference/reference.py
@@ -1,1 +1,20 @@
+print('Простое присваивание')
+
+shoplist = ['яблоки','манго','морковь','бананы']
+mylist = shoplist
+
+del(shoplist[0])
+
+print('shoplist: ',shoplist)
+print('mylist: ',mylist)
+
+print('Копирование при помощи полной вырезки')
+print()
+
+mylist = shoplist[:]
+del mylist[0]
+print('shoplist: ',shoplist)
+print('mylist: ',mylist)
+
+
 
